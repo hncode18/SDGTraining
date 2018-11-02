82809628e7a15b58966448c8e4516a618bc547f1
Hoang Nguyen <nguyeho2@oregonstate.edu>
2018-11-02 13:14:10 -0700

    Hooked up database to Web applications

---
+++ PeopleProTraining/PeopleProTraining/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace PeopleProTraining.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public double wage { get; set; }
+        public string position { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
