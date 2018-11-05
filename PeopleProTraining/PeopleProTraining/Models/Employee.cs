13b23ec20f44712a3b9d6ead527a042d8404277f
Hoang Nguyen <nguyeho2@oregonstate.edu>
2018-11-05 12:50:28 -0800

    Format Headings for Employee Listing

--- PeopleProTraining/PeopleProTraining/Models/Employee.cs
+++ PeopleProTraining/PeopleProTraining/Models/Employee.cs
@@ -1,12 +1,17 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
 
 namespace PeopleProTraining.Models
 {
     public class Employee
     {
         public int ID { get; set; }
+        [Display(Name = "Name")]
         public string name { get; set; }
+        [Display(Name = "Wage")]
         public double wage { get; set; }
+        [Display(Name = "Position")]
         public string position { get; set; }
     }
 
