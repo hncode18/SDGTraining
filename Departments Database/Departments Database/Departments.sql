f1f00f511c55e93317062d884893dc679656b977
Hoang Nguyen <nguyeho2@oregonstate.edu>
2018-11-06 11:26:29 -0800

    Set up table for departments

---
+++ Departments Database/Departments Database/Departments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Departments]
+(
+	[DepartmentID]	INT IDENTITY (1, 1) NOT NULL,
+	[Name]      NVARCHAR (50) NULL, 
+    [Expenses] FLOAT NULL, 
+    [Employees] INT NULL,
+	
+
+)
