2b259089baba3ece9571a14c6905b6b24b8a1926
Hoang Nguyen <nguyeho2@oregonstate.edu>
2018-11-08 15:53:37 -0800

    Add  begining of Post Deployment script for Database project

---
+++ Departments Database/Departments Database/Script.Post_Deploy.sql
@@ -0,0 +1,24 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO Employees AS Target
+USING (VALUES
+		(1, 'Hoang Nguyen', 11.25, 'Programmer', 1),
+		(2, 'John Henry', 11.25, 'Steel Driver', 2),
+		(3, 'Harold', 60, 'Manager', 1),
+		(4, 'Thomas Paine', 200, 'Lawyer', 2)
+)
+AS Source (ID, Name, Wage, Position, DepartmentID)
+ON Target.ID = Source.ID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (Name, Wage, Position, DepartmentID)
+VALUES (Name, Wage, Position, DepartmentID);
